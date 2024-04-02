f31410b46c0801d875450e1131f2b54777e7d079
Thuận Phát <phat06112003@gmail.com>
2024-04-02 22:03:32 +0700

    add git ignore file

--- obj/Debug/net8.0-windows/Caro-game.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Caro-game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Caro-game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09b25f00b264f91ea9a9dcf2553a41d69a3372b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Caro-game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Caro-game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
